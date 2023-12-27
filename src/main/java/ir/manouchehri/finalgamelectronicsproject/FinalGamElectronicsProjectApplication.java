423ef84ddb603735f6efbeae042c272343b91064
ariya <ariyamanouchehri079@gmail.com>
2023-12-27 10:04:38 +0330

    Initial commit

---
+++ src/main/java/ir/manouchehri/finalgamelectronicsproject/FinalGamElectronicsProjectApplication.java
@@ -0,0 +1,13 @@
+package ir.manouchehri.finalgamelectronicsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalGamElectronicsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalGamElectronicsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/manouchehri/finalgamelectronicsproject/FinalGamElectronicsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.manouchehri.finalgamelectronicsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalGamElectronicsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
